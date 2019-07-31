ff387aa9eaa22e2127a1c96c58d1dd7116e0ccd5
maurogarcesd <maurogarcesd@gmail.com>
2019-07-31 01:10:02 -0500

    Funcionando car y add car

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CarListComponent } from './car-list/car-list.component';
 import { CarEditComponent } from './car-edit/car-edit.component';
+import { OwnerListComponent } from './owner-list/owner-list.component';
+import { OwnerEditComponent } from './owner-edit/owner-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/car-list', pathMatch: 'full' },
@@ -9,6 +11,19 @@ const routes: Routes = [
     path: 'car-list',
     component: CarListComponent
   },
+  {
+    path: 'owner-list',
+    component: OwnerListComponent
+  },
+  {
+    path: 'owner-add',
+    component: OwnerEditComponent
+  },
+  {
+    path: 'owner-edit/:href',
+    component: OwnerEditComponent
+  },
+  
   {
     path: 'car-add',
     component: CarEditComponent
